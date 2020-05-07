400934e62ee3cdbb22ea96eb930ddcb8e9b2f93f
Dharini S <shah.dharin@husky.neu.edu>
2020-05-07 01:48:02 -0400

    database connection to heroku

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-    host : process.env.DATABASE_URL,
-    ssl: true
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
