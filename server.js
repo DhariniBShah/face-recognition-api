4023f58f2d25941662c00866e11712af95db34da
Dharini S <shah.dharin@husky.neu.edu>
2020-05-07 00:34:17 -0400

    updated the port for deployment

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ const db = knex({
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`app is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`app is running on port ${PORT}`);
 })
 
 app.use(bodyParser.json());
